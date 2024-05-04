a33d933ff6cb08cda6beaa1773a19f523a58da68
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-05-04 11:00:20 +0930

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    employeeName: "Stefan Kasearu",
+    Address: "11 Cldye Road"
+};
+
+// Function to non-destructively update employee
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+
+// Function to destructively update employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// Function to non-destructively delete a key from employee
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// Function to destructively delete a key from employee
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
